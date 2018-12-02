de5ac41cb9b94bb2610338c96624f4284da24021
OlgaKopersako <44473944+OlgaKopersako@users.noreply.github.com>
2018-12-02 18:06:59 +0200

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -1,7 +1,7 @@
 <?
 if (isset ($_POST['messageFF'])) {
   mail ("o.kopersako@gmail.com",
-        "заполнена контактная форма "."https://github.com/OlgaKopersako/olgakopersako.github.io/contacts.php=".urlencode("https://github.com/OlgaKopersako/olgakopersako.github.io/index.html")
+        "заполнена контактная форма "."https://github.com/OlgaKopersako/olgakopersako.github.io/contacts.php?prevUrl=".urlencode("https://github.com/OlgaKopersako/olgakopersako.github.io/index.html")
         "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nСообщение: ".$_POST['messageFF']);
   echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
 }
