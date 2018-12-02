53ac5238baf5070fc44e16fe4675f8edcaaac4b4
OlgaKopersako <44473944+OlgaKopersako@users.noreply.github.com>
2018-12-02 17:41:09 +0200

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -1,6 +1,6 @@
 <?
 if (isset ($_POST['messageFF'])) {
-  mail ("свой@yandex.ru",
+  mail ("o.kopersako@gmail.com",
         "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
         "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nСообщение: ".$_POST['messageFF']);
   echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
